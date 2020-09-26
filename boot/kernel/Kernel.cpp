5df770f84766b59ad69883a4b2ababf49cb3e25e
Yotam Salmon <yotam.salmon@gmail.com>
2020-09-26 12:21:40 +0300

    Create a linker script to really link and compile the kernel correctly

--- boot/kernel/Kernel.cpp
+++ boot/kernel/Kernel.cpp
@@ -1,5 +1,14 @@
 #include "Ktype.hpp"
 
+/*
+TODO: This kernel is currently embedded into the bootloader. This is not very good, but it
+simplifies the kernel loading process (otherwise the bootloader 32 bit code should have had
+drivers to read from the disk).
+
+Next, we should really port the kernel to work with the multiboot standard, so it can be loaded
+by a pre-made bootloader such as GRUB.
+*/
+
 typedef struct
 {
     char Char;
