ebcabb56be39e881797da39946ad59e1dd17ffbc
Yotam Salmon <yotam.salmon@gmail.com>
2020-09-27 15:28:35 +0300

    Add __volatile__ on __asm__ (important now when I intend to use it)

--- kernel/arch/x86/io/Io.cpp
+++ kernel/arch/x86/io/Io.cpp
@@ -3,7 +3,7 @@
 BYTE IoPortByteIn(const UINT16 port)
 {
     unsigned char result;
-    __asm__(
+    __asm__ __volatile__(
         "in %%dx, %%al"
         : "=a"(result)
         : "d"(port));
@@ -13,14 +13,14 @@ BYTE IoPortByteIn(const UINT16 port)
 
 void IoPortByteOut(const UINT16 port, const BYTE value)
 {
-    __asm__(
+    __asm__ __volatile__(
         "out %%al, %%dx" ::"a"(value), "d"(port));
 }
 
 UINT16 IoPortWordIn(const UINT16 port)
 {
     UINT16 result;
-    __asm__(
+    __asm__ __volatile__(
         "in %%dx, %%ax"
         : "=a"(result)
         : "d"(port));
@@ -29,6 +29,6 @@ UINT16 IoPortWordIn(const UINT16 port)
 
 void IoPortWordOut(const UINT16 port, const UINT16 value)
 {
-    __asm__(
+    __asm__ __volatile__(
         "out %%ax, %%dx" ::"a"(value), "d"(port));
 }
