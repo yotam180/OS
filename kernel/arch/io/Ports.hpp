75b0dea104aaff97de16276231b4b57420fe54cb
Yotam Salmon <yotam.salmon@gmail.com>
2020-09-28 13:32:34 +0300

    Clean up Github code from repository

--- kernel/arch/io/Ports.hpp
+++ kernel/arch/io/Ports.hpp
@@ -9,8 +9,3 @@ void PortByteOut(const UINT16 port, const BYTE value);
 UINT16 PortWordIn(const UINT16 port);
 void PortWordOut(const UINT16 port, const UINT16 value);
 } // namespace Io
-
-unsigned char port_byte_in(unsigned short port);
-void port_byte_out(unsigned short port, unsigned char data);
-unsigned short port_word_in(unsigned short port);
-void port_word_out(unsigned short port, unsigned short data);
